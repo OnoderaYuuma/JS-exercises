6586bd7e2d46553d15f7ce2b3a85935877379d37
OnoderaYuuma <y.onodera.sys23@morijyobi.ac.jp>
2024-03-25 19:50:23 +0900

    practice6

---
+++ practice6/practice6.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+
+function btnClick1() {
+    let text = document.getElementById("form").value;
+    let changeText = document.getElementById("text");
+    changeText.textContent = text;
+    changeText.className = "";
+}
+function btnClick2() {
+    let text = document.getElementById("form").value;
+    let changeText = document.getElementById("text");
+    changeText.textContent = text;
+
+    changeText.className = "style1";
+}
+function btnClick3() {
+    let text = document.getElementById("form").value;
+    let changeText = document.getElementById("text");
+    changeText.textContent = text;
+
+    changeText.className = "style2";
+}
+function btnClick4() {
+    let text = document.getElementById("form").value;
+    let changeText = document.getElementById("text");
+    changeText.textContent = text;
+
+    changeText.className = "style3";
+}
+function btnClick5() {
+    let text = document.getElementById("form").value;
+    let changeText = document.getElementById("text");
+    changeText.textContent = text;
+
+    changeText.className = "style4";
+}
